326469a6a309b8f0bfd9483f2c64bb5b4361f8f6
Freddy O.Figueroa L <42115718+FreddyOrlandoFL@users.noreply.github.com>
2023-12-06 11:55:20 -0400

    migraciones

---
+++ database/migrations/2023_12_06_151827_assing_diagnosis.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assing_diagnostics', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient'); 
+            $table->foreign('patient')->references('id')->on('pacients');
+            $table->unsignedBigInteger('diagnostic');
+            $table->foreign('diagnostic')->references('id')->on('diagnostics');
+            $table->string('observation',255);
+            $table->string('creation',255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('assing_diagnostics');
+    }
+};
